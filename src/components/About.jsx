5c1873488cc07a853c2dd77d01c6aaa0146fe027
likhilavyd <likhila.skilldevelopment@gmail.com>
2025-08-01 23:41:29 +0530

    Added About and Header components with navigation and intro; included CSS files for styling.

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div>
+      <h2 className="">About Me</h2>
+      <p className="text-center">
+        Hi, I'm Likhila Vydana — a student and passionate web developer with a strong interest in 
+        clean UI and interactive design. I focus on building responsive, user-friendly websites that 
+        combine functionality with visual appeal. My journey began with a curiosity about how websites 
+        work behind the scenes, and it has grown into a meaningful path in front-end development. 
+        As I continue to learn and explore new technologies, I’m excited to turn creative ideas into impactful digital experiences.
+      </p>
+    </div>
+  )
+}
+export default About;
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+function Header() {
+  return (
+        <header className="bg-dark text-white py-3 sticky-top z-3 ml-0">
+        <nav className="container d-flex justify-content-between align-items-center">
+            <h1 className="fs-4 m-0">Likhila Vydana</h1>
+            <ul className="nav gap-3">
+            <li className="nav-item">
+                <a className="nav-link text-white fw-medium" href="#about">About</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-white fw-medium" href="#personal details">Personal Details</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-white fw-medium" href="#resume">Resume</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-white fw-medium" href="#skills">Skills</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-white fw-medium" href="#projects">Projects</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-white fw-medium" href="#experience">Experience</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-white fw-medium" href="#contact">Contact</a>
+            </li>
+            </ul>
+        </nav>
+        </header>
+
+  )
+}
+
+export default Header;
